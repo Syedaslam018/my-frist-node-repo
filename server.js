8ef19bd2ee19646a6c0b5f4059fc6e358df560b5
Syed Aslam <syedaslam.me@gmail.com>
2023-06-11 14:22:33 +0530

    add some html to my server response

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require('http')
 
 const server = http.createServer((req,res) => {
-    console.log('Syed Aslam')
+    // console.log(req.headers, req.url, req.method)
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>some page i created for my server</title></head>')
+    res.write('<body><h1>Hello from my node js server<h1></body>')
+    res.write('')
+    res.write('</html>')
+    res.end()
 })
 
 server.listen(4000)
