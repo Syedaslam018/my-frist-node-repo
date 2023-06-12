5310d955817da226f87e5e4d65471b989d6fabed
Syed Aslam <syedaslam.me@gmail.com>
2023-06-12 12:43:03 +0530

    add routes to add product and display

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 
 const express  = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('inside the middleware')
-    next();
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product',(req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next) => {
-    console.log('inside another middleware')
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+    
     res.send('<h1>Hello World!<h1>')
 })
 
