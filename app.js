e505090a999ee857101ec88ebc4a3385daafe719
Syed Aslam <syedaslam.me@gmail.com>
2023-06-15 17:46:47 +0530

    add ejs resources

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
-
-const express  = require('express'); 
 const path = require('path');
 
-const adminRoutes = require('./routes/admin')
-
-const shopRoutes = require('./routes/shop')
-const contactusRoute = require('./routes/contactus')
-const successRoute = require('./routes/success')
-
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use('/admin',adminRoutes)
-app.use('/shop',shopRoutes)
-app.use('/contactus', contactusRoute);
-app.use('/success', successRoute)
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
-app.listen(4000)
+app.listen(3000);
